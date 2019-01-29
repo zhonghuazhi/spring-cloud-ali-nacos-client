9a7002c5ffcfffdac28416cf72c7a7dd67f21521
zhizhonghua <zhizhonghua@boco.com.cn>
2019-01-29 20:41:04 +0800

    版本提交

---
+++ src/main/java/net/cc/springcloud/ali/nacos/client/SpringCloudAliNacosClientApplication.java
@@ -0,0 +1,42 @@
+package net.cc.springcloud.ali.nacos.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudAliNacosClientApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(SpringCloudAliNacosClientApplication.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudAliNacosClientApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+
+        @Autowired
+        LoadBalancerClient loadBalancerClient;
+
+        @GetMapping("/test")
+        public String test() {
+            // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
+            ServiceInstance serviceInstance = loadBalancerClient.choose("spring-cloud-ali-nacos-server");
+            String url = serviceInstance.getUri() + "/hello";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(url, String.class);
+            return "Invoke : " + url + ", return : " + result;
+        }
+    }
+}
